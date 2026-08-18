ef773fa90b9a9ca2530e09531e2e5c77fbd14438
smruti <gondkarsmruti@gmail.com>
2026-08-18 16:13:45 +0000

    Hello World Using LangGraph

---
+++ 01.Hello_world_agent_8_July/4.hello_world_node2_first.py
@@ -0,0 +1,37 @@
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph,START,END
+
+class GreetingState(TypedDict):
+    greeting:str
+    sample_var:int
+
+def node1(state:GreetingState):
+    state["greeting"]=state["greeting"]+" , How are you?"
+    return state 
+
+def node2(state:GreetingState):
+    state["greeting"]=state["greeting"]+"!"
+    return state
+
+builder=StateGraph(GreetingState)
+
+builder.add_node("node1",node1)
+builder.add_node("node2",node2)
+
+builder.add_edge(START,"node2")
+builder.add_edge("node2","node1") 
+builder.add_edge("node1",END)
+
+
+graph=builder.compile()
+
+
+result=graph.invoke({
+    "greeting":"Good Morning"
+})
+
+
+print(result)
+
+print(graph.get_graph().draw_ascii())
+
